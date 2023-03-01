3f720dde0353d3eaba8bc95aef0e66896f2bd2f7
wasif9700 <wasifmian4@gmail.com>
2023-03-02 02:01:46 +0500

    sumFunction

--- Index.js
+++ Index.js
@@ -1,22 +1,7 @@
- HEAD
- HEAD
 const sum = (x,y)=>{
     let res = x+y;
     console.log("Result = ",res);
     return res;
 }
 
- const divide =(x, y) => {
-    let res = x / y;
-    console.log("Result = ", res);
-    return res;
-  }
- Divi
 
-const multi =(x, y) => {
-    let res = x * y;
-    console.log("Result = ", res);
-    return res;
-  }
-  
- Multi
